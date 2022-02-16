ecdd8af284ce3307ef7356ccafaad601fa48c258
Acho <aonunwor@yahoo.com>
2022-02-16 11:42:28 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send(`Inventory_app Home page`)
+    res.end();
+})
+
+
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, ()=>{
+        console.log(`APP is listening on port ${PORT}!`);
+        console.log(`press ctrl+c to quit`);
+})
