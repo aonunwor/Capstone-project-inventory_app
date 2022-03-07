4f446a715115d476de6cdb300f9f326d744cbd28
Acho <aonunwor@yahoo.com>
2022-03-07 16:07:21 +0100

    Database added

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const { Console } = require('console');
+const bodyParser = require('body-parser');
+const User = require(`./models/user`);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 
+main().catch(err =>{
+        console.log("OH NO MONGO CONNECTION ERROR!!!");
+        console.log(err);
+});
+
+async function main(){
+        await mongoose.connect('mongodb://localhost:27017/purchaseDB', {useNewUrlParser: true, useUnifiedTopology:true})
+};
 
 app.get('/', (req, res)=>{
     res.status(200);
@@ -23,6 +36,49 @@ app.get('/register', (req,res)=>{
         res.end();
 })
 
+//To Login
+app.get('/login', (req,res)=>{
+        res.status(200);
+        res.render(`pages/login`);
+        res.end();
+})
+
+//To submit registered page
+app.post('/register', async(req,res) =>{
+        try{
+                const password = req.body.Password;
+                const cpassword = req.body.ConfirmPassword;
+                if(password === cpassword){
+
+                        const registerUser = new User({
+                                Firstname : req.body.Firstname,
+                                Lastname : req.body.Lastname,
+                                Email : req.body.Email,
+                                PhoneNumber : req.body.PhoneNumber,
+                                Gender : req.body.Gender,
+                                DOB : req.body.DOB,
+                                Address: req.body.Address,
+                                City: req.body.City,
+                                State: req.body.State,
+                                Password: password,
+                                ConfirmPassword : cpassword
+                        });
+
+                        const registered = await registerUser.save();
+                        res.status(201);
+                        res.render('pages/login');
+                        res.end();     
+                }else{
+                        const script = `<script>alert("Both passwords do not match").window.location.href = "/register"</script>`;
+                        res.send(script);
+                }
+        }catch(error){
+                res.status(400);
+                res.send(error);
+        }
+        
+        
+})
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, ()=>{
         console.log(`APP is listening on port ${PORT}!`);
---
+++ models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require(`mongoose`);
+const { Schema } = mongoose;
+
+// create a schema for register form
+
+const userSchema = new Schema(
+        {
+                Firstname:{
+                        type: String,
+                        required: true,
+                },
+                Lastname:{
+                    type: String,
+                    required: true,
+                },
+                Email:{
+                    type: Schema.Types.Mixed,
+                    required: true,
+                },
+                PhoneNumber:{
+                    type: Number,
+                    required: true,
+                },
+                Gender:{
+                    type: String,
+                    //enum: [`Male`, `Female`, `Dash`]
+                },
+                DOB:{
+                    type: Date,
+                    required: true,
+                },
+                Address:{
+                    type: String,
+                    maxlength: 50,
+                    required: true,
+                },
+                City:{
+                    type: String,
+                    maxlength: 20,
+                    required: true,
+                },
+                State:{
+                    type: String,
+                    maxlength: 30,
+                    required: true,
+                },
+                Password:{
+                    type: Schema.Types.Mixed,
+                    required: true,
+                    minlength: 20
+                },
+                ConfirmPassword:{
+                    type: Schema.Types.Mixed,
+                    required: true,
+                    minlength: 20
+                },
+        },
+        {
+                timestamps: true,
+        }
+)
+
+//Create Model
+const User= mongoose.model('User', userSchema);
+
+//Export Model
+module.exports = User;
